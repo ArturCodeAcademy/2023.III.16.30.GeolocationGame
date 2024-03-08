b77e99bff1d2b7d48d87b738c0d926dc165f3586
Artur <Artur@email.com>
2024-03-08 17:11:01 +0200

    Add city count selector

---
+++ Assets/Scripts/UI/CityCountSelector.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CityCountSelector : MonoBehaviour
+{
+	public UnityEvent<int> Approved;
+
+	public const int MIN_CITY_COUNT = 1;
+	public const int MAX_CITY_COUNT = 25;
+
+	[SerializeField] private TMP_InputField _cityCountInputField;
+	[SerializeField] private Button _aprooveButton;
+
+	private string _cityCountString = "5";
+
+	private void Awake()
+	{
+		_cityCountInputField.text = _cityCountString;
+		Approved ??= new UnityEvent<int>();
+	}
+
+	private void OnEnable()
+	{
+		_cityCountInputField.onValueChanged.AddListener(OnCityCountChanged);
+		_aprooveButton.onClick.AddListener(OnApproveButtonClicked);
+	}
+
+	private void OnDisable()
+	{
+		_cityCountInputField.onValueChanged.RemoveListener(OnCityCountChanged);
+		_aprooveButton.onClick.RemoveListener(OnApproveButtonClicked);
+	}
+
+	private void OnCityCountChanged(string value)
+	{
+		_cityCountString = value;
+	}
+
+	private void OnApproveButtonClicked()
+	{
+		if (int.TryParse(_cityCountString, out int count))
+		{
+			if (count >= MIN_CITY_COUNT && count <= MAX_CITY_COUNT)
+			{
+				Approved.Invoke(count);
+				return;
+			}
+		}
+
+		count = Mathf.Clamp(count, MIN_CITY_COUNT, MAX_CITY_COUNT);
+		_cityCountInputField.text = count.ToString();
+	}
+}
